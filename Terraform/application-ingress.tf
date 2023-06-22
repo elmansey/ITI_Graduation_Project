c04baf3a094e6e7cb7e80b3efaed53bf60ddbcc7
elmansey <almanseyalmansey6@gmail.com>
2023-06-22 15:28:15 +0300

    ---

---
+++ Terraform/application-ingress.tf
@@ -0,0 +1,26 @@
+resource "kubernetes_ingress" "app_ingress" {
+  metadata {
+    name = "app-ingress"
+    namespace = "${kubernetes_namespace.tools.metadata.0.name}"
+    annotations = {
+      "kubernetes.io/ingress.class" = "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "minikube.local"
+      http {
+        path {
+          path = "/nexus"
+
+          backend {
+            service_name = kubernetes_service.nexus_svc.metadata[0].name
+            service_port = kubernetes_service.nexus_svc.spec[0].port[0].port
+          }
+        }
+      }
+    }
+  }
+}
+
