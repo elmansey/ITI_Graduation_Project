2e3f69401b86cc361883c90c357d9a407e7ae7f8
mohammedelnabawy <mohammedelnabawy11@gmail.com>
2023-06-27 15:12:49 +0300

    add docker file

--- Terraform/application-ingress.tf
+++ Terraform/application-ingress.tf
@@ -1,26 +1,26 @@
-resource "kubernetes_ingress" "app_ingress" {
-  metadata {
-    name = "app-ingress"
-    namespace = "${kubernetes_namespace.tools.metadata.0.name}"
-    annotations = {
-      "kubernetes.io/ingress.class" = "nginx"
-    }
-  }
+# resource "kubernetes_ingress" "app_ingress" {
+#   metadata {
+#     name = "app-ingress"
+#     namespace = "${kubernetes_namespace.tools.metadata.0.name}"
+#     annotations = {
+#       "kubernetes.io/ingress.class" = "nginx"
+#     }ku
+#   }
 
-  spec {
-    rule {
-      host = "minikube.local"
-      http {
-        path {
-          path = "/nexus"
+#   spec {
+#     rule {
+#       host = "minikube.local"
+#       http {
+#         path {
+#           path = "/nexus"
 
-          backend {
-            service_name = kubernetes_service.nexus_svc.metadata[0].name
-            service_port = kubernetes_service.nexus_svc.spec[0].port[0].port
-          }
-        }
-      }
-    }
-  }
-}
+#           backend {
+#             service_name = kubernetes_service.nexus_svc.metadata[0].name
+#             service_port = kubernetes_service.nexus_svc.spec[0].port[0].port
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
 
---
+++ dockerfile
@@ -0,0 +1,12 @@
+FROM jenkins/jenkins:latest
+USER root
+
+RUN apt update && \
+    apt install apt-transport-https ca-certificates curl software-properties-common unzip -y
+
+RUN curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+RUN add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+RUN apt install docker.io -y
+
+RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+RUN install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
